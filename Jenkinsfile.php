febe7eed77a6221f31f96b2bf752aa975027fc64
gautamkunjadiya99 <gautamkunjadiya99@gmail.com>
2026-06-16 23:03:49 +0530

    initial commit

---
+++ Jenkinsfile.php
@@ -0,0 +1,38 @@
+pipeline {
+    agent any
+    stages {
+        stage('Checkout Code') {
+            steps {
+                git branch: 'main', url: 'https://github.com/gautamkunjadiya99/Testqa.git'
+            }
+        }
+        stage('Install Dependencies') {
+            steps {
+                sh 'npm install'
+            }
+        }
+        stage('Install Playwright Browsers') {
+            steps {
+                sh 'npx playwright install --with-deps'
+            }
+        }
+        stage('Run Playwright Tests') {
+            steps {
+                sh 'npx playwright test'
+            }
+        }
+    }
+    post {
+        always {
+            echo 'Publishing Playwright report...'
+        }
+
+        success {
+            echo 'Tests PASSED ✅'
+        }
+
+        failure {
+            echo 'Tests FAILED ❌'
+        }
+    }
+}
